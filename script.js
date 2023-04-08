bd369b0544d22247f9b69a468d26d6e9f193a0dd
Jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2023-04-09 01:42:13 +0530

    Intial changes

--- script.js
+++ script.js
@@ -132,3 +132,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// github
+
