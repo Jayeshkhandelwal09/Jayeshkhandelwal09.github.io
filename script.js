e6748e892c169c890a0011fdde5c3fef32dadaf4
Jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2023-04-19 18:00:17 +0530

    Skills,projects and pdf opening solved

--- script.js
+++ script.js
@@ -142,4 +142,9 @@ document.getElementById('resume-link-1').addEventListener("click", () => {
     console.log("OPENinig.....");
     window.open("https://drive.google.com/file/d/1I7WJjZsAarVpxKsTsWO-SyuFsGZL6rcU/view?usp=share_link", "_blank");
   });
+  document.getElementById('resume-link-3').addEventListener("click", () => {
+    console.log("OPENinig.....");
+    window.open("https://drive.google.com/file/d/1I7WJjZsAarVpxKsTsWO-SyuFsGZL6rcU/view?usp=share_link", "_blank");
+  });
+
 
