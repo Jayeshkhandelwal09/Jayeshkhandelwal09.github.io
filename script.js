a0cf75cd2feef17770c97199eeda48ac45e6b3dd
jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2025-01-23 19:39:38 +0530

    Some changes

--- script.js
+++ script.js
@@ -144,12 +144,12 @@ themeButton.addEventListener('click', () => {
 
 // Resume opening links
 document.getElementById('resume-button-1').addEventListener("click", () => {
-    window.open("https://drive.google.com/file/d/1I7WJjZsAarVpxKsTsWO-SyuFsGZL6rcU/view?usp=drive_link", "_blank");
+    window.open("https://drive.google.com/file/d/1QMI3P0NQa9rubjgiCDqhNVMk_XN-hBj9/view?usp=sharing", "_blank");
   });
   
   document.getElementById('resume-button-2').addEventListener("click", () => {
     console.log("OPENinig.....");
-    window.open("https://drive.google.com/file/d/1I7WJjZsAarVpxKsTsWO-SyuFsGZL6rcU/view?usp=drive_link", "_blank");
+    window.open("https://drive.google.com/file/d/1QMI3P0NQa9rubjgiCDqhNVMk_XN-hBj9/view?usp=sharing", "_blank");
   });
 
 
